ed57e4f40cc95325ade0251a76dd709e8b6241a3
Iori Minami <iori.minami@gmail.com>
2018-02-05 16:04:03 +0900

    Initial Commit

---
+++ BandAssignPro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BandAssignPro
+//
+//  Created by 南伊織 on 2018/02/02.
+//  Copyright © 2018年 南伊織. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
